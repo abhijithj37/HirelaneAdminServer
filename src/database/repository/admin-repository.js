4c732719689988735891348a891488daa944ce2f
Abhijith J <abhijithj47s0@gmail.com>
2023-06-14 09:19:48 +0530

    first commit

---
+++ src/database/repository/admin-repository.js
@@ -0,0 +1,17 @@
+
+const {admin} =require('../model')
+
+
+module.exports={
+
+    findAdmin:async(userName,password)=>{
+    const adminData=await admin.findOne({userName:userName,password})
+     return adminData
+    },
+    findAdminById:async(id)=>{
+   const data=await admin.findOne({_id:id})
+   return data
+    }
+
+
+}
